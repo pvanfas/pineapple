1f442a2d75e4efefc30e95eed42996637b7eda6c
Anfas PV <anfaspv.info@gmail.com>
2022-10-08 00:04:02 +0400

    initial commit

---
+++ pineapple/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pineapple.users.models import User
+from pineapple.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
